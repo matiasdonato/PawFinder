7843e912599efb286431eeb08310442a85099056
Matias <matiasdonato14@gmail.com>
2023-03-07 01:11:35 -0300

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const fs = require('fs');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/", (req, res) => {
+    res.send("Hello World")
+})
+
+//aplying middlewares
+function loadFunctionsFromFolder(folderPath) {
+    fs.readdirSync(folderPath)
+        .filter((file) => file.endsWith('.js'))
+        .forEach((file) => {
+            const filePath = (folderPath + "/" + file).split("/");
+            const route = "./routes/" + filePath[3]
+            let middlew = require(route)
+            app.use("/" + filePath[3].split(".")[0], middlew)
+        });
+}
+loadFunctionsFromFolder("./src/routes")
+
+module.exports = app;
